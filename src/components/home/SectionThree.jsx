28e8c68db8ef2d298eff3c09d34b5d70771fccd8
AgustioMaitimu <agustiomaitimu8@gmail.com>
2024-07-10 01:10:20 +0700

    style: fixed section three styling

--- src/components/home/SectionThree.jsx
+++ src/components/home/SectionThree.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SectionThree() {
   return (
-    <div className="mx-6 w-full max-w-[1000] rounded-br-3xl rounded-tl-3xl bg-[#2BA3FF] p-6 text-center text-[26px] font-light text-white lg:rounded-br-full lg:rounded-tl-full lg:text-3xl">
+    <div className="w-[87%] max-w-[1000] rounded-br-3xl rounded-tl-3xl bg-[#2BA3FF] p-6 text-center text-[26px] font-light text-white midway:w-[95%] lg:w-full lg:rounded-br-full lg:rounded-tl-full lg:text-3xl">
       Bring your data and code, we do the rest.
     </div>
   );
