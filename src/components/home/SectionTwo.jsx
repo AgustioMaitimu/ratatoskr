7875cf64b745ecb2d8fa780ebc4085148190253b
AgustioMaitimu <agustiomaitimu8@gmail.com>
2024-07-11 18:43:14 +0700

    feat: removed fade up animation from section 2

--- src/components/home/SectionTwo.jsx
+++ src/components/home/SectionTwo.jsx
@@ -13,7 +13,7 @@ function SectionTwo() {
   const [pickedExample, setPickedExample] = useState('Branch');
 
   return (
-    <div className="fade-up flex w-full max-w-[1200px] flex-col items-center gap-5 bg-[#F7F7F7] bg-[url('/background-1.png')] bg-cover px-6 py-36 midway:rounded-2xl">
+    <div className="flex w-full max-w-[1200px] flex-col items-center gap-5 bg-[#F7F7F7] bg-[url('/background-1.png')] bg-cover px-6 py-36 midway:rounded-2xl">
       <div
         className={`flex w-full max-w-[350px] justify-evenly rounded-full bg-black p-2 font-light text-white midway:px-1 midway:py-2`}
       >
