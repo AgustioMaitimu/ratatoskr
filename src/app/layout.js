03e268def834e5d401d83a97e90efcdb8ac3456b
AgustioMaitimu <agustiomaitimu8@gmail.com>
2024-07-21 04:55:42 +0700

    feat: added site title

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,7 @@ import Footer from '@/components/Footer';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <title>Sqrel | Interactive Single-Page App Showcase</title>
       <body className="z-0 flex flex-col items-center">
         <Header />
         {children}
